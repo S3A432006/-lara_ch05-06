904a9a146014e1ff68029d01ed6eb7b581882cbe
Jia  Yi Lai <anniuu7134@gmail.com>
2017-10-24 13:09:32 +0800

    練習四: 正規表達式限制參數

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+/*
 //練習一: 顯示學生資料和成績
 Route::get('student/{student_no}',function ($student_no){
     return '學號：'.$student_no;
@@ -29,3 +30,11 @@ Route::get('student/{student_no}/score/{subject}', function ($student_no,$subjec
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 });
+*/
+//練習四: 正規表達式限制參數
+Route::get('student/{student_no}',function ($student_no){
+    return '學號：'.$student_no;
+}) -> where(['student_no' => 's[0-9]{10}']);
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
+    return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
+}) -> where(['student_no' => 's[0-9]{10}','subject' => '(chinese|english|math)']);
